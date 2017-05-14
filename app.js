9e359552369c1360804c4d673dea64ec8c3e363a
praveen <praveenoboroy1576@gmail>
2017-05-14 03:49:28 -0700

    third testing bot

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: '74f07719-d37a-4683-9e86-6fdde7b307bc', appPassword: 'UB1xngHNr5biL8q12bydbiD' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
